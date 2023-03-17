8d577e9a22d5a8c3ab041eb54021d2e228b2abc5
sree7k7 <sree7k7@gmail.com>
2023-03-17 19:07:15 +0100

    commit

---
+++ github_codepipe/github_codepipe_stack.py
@@ -0,0 +1,41 @@
+import aws_cdk as cdk
+from aws_cdk import Stack  # Duration,; aws_sqs as sqs,
+from aws_cdk import aws_codecommit as codecommit
+import aws_cdk.aws_ec2 as ec2
+from aws_cdk.pipelines import ManualApprovalStep
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
+from constructs import Construct
+from aws_cdk import (
+    # Duration,
+    Stack,
+    # aws_sqs as sqs,
+)
+
+class GithubCodepipeStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        self.backend_repository =  codecommit.Repository.from_repository_name(
+            self,
+            "github-to-aws",
+            repository_name="github-codepipeline" # aws codecommit repository name
+        )
+        pipeline = CodePipeline(
+            self,
+            "github-to-codepipeline",
+            pipeline_name=self.backend_repository.repository_name,
+            cross_account_keys=True,
+            self_mutation=True,
+            synth=ShellStep("Synth",
+                            input=CodePipelineSource.code_commit(
+                            repository=self.backend_repository, 
+                            branch="master"
+                            ),
+                            commands=[
+                                "npm install -g aws-cdk",
+                                "python -m pip install -r requirements.txt",
+                                "cdk synth"
+                            ]
+                            )        
+        )  
